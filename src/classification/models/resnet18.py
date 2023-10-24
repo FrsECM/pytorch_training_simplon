7d5569f40cfc44d775f4a9274fd2c52e77f9a98f
François Ponchon <francois.ponchon@michelin.com>
2023-10-24 11:04:00 +0200

    Create Model Resnet18

--- src/classification/models/resnet18.py
+++ src/classification/models/resnet18.py
@@ -13,8 +13,7 @@ class ConvBlock(nn.Module):
         y = self.act(y)
         return y
 
-
-class ResnetBlock(nn.Module):
+class ResBlock(nn.Module):
     def __init__(self,
                  in_channels:int,
                  out_channels:int,
@@ -38,15 +37,44 @@ class ResnetBlock(nn.Module):
         return y
 
 class Resnet18(nn.Module):
-    def __init__(self):
+    def __init__(self,in_channels:int,nClasses:int):
         super().__init__()
-        self.conv1=nn.Conv2d(3,64,kernel_size=7,stride=2,padding=3)
+        self.nClasses=nClasses
+        self.conv1=ConvBlock(in_channels,64,kernel_size=7,stride=2,padding=3)
+        self.conv2_x = nn.Sequential(
+            nn.MaxPool2d(kernel_size=3,stride=2,padding=1),
+            ResBlock(64,64),
+            ResBlock(64,64)
+        )
+        self.conv3_x = nn.Sequential(
+            ResBlock(64,128,stride=2),
+            ResBlock(128,128),
+        )
+        self.conv4_x = nn.Sequential(
+            ResBlock(128,256,stride=2),
+            ResBlock(256,256),
+        )
+        self.conv5_x = nn.Sequential(
+            ResBlock(256,512,stride=2),
+            ResBlock(512,512),
+        )
+        self.head = nn.Sequential(
+            nn.AdaptiveAvgPool2d(output_size=(1,1)),
+            nn.Flatten(),
+            nn.Linear(512,self.nClasses)
+        )
     def forward(self,x):
-        pass
+        y1 = self.conv1(x)
+        y2 = self.conv2_x(y1)
+        y3 = self.conv3_x(y2)
+        y4 = self.conv4_x(y3)
+        y5 = self.conv5_x(y4)
+        y = self.head(y5)
+        return y
 
 
 if __name__=='__main__':
     input = torch.randn((4,3,224,224))
-    model = ResnetBlock(3,64,stride=2)
+    model = Resnet18(3,20)
     output = model(input)
     print('terminé')
